91273a930c9db8314deed177e5c11183ea006401
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-07-17 19:52:07 -0300

    Topicos de Big data em Python Regex

---
+++ principal.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+import re
+
+palavra_regex = re.compile(r"[\w']+")
+
+
+class QuantidadePalavras(MRJob):
+    def mapper(self, _, linha):
+        for p in palavra_regex.findall(linha):
+            yield (p.lower(), 1)
+
+    def reducer(self, p, qtd):
+        yield (p, sum(qtd))

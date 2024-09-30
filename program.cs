9759e9d87f4f14ceeb7dc2596c5b157649b55348
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-09-29 22:18:49 -0300

    Create program.cs
    Modelagem de Sistemas em UML trabalhando com desenvolvimento em camadas utilizando C sharp

---
+++ program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace DAO
+{
+    public class Dao
+    {
+        public List<string> RetornaListaEstado()
+        {
+            List<string> listaEstados = new List<string>();
+            listaEstados.Add("Acre");
+            listaEstados.Add("Alagoas");
+            listaEstados.Add("Alagoas");
+            listaEstados.Add("Alagoas");
+            listaEstados.Add("Amapá");
+            listaEstados.Add("Amazonas");
+            listaEstados.Add("Bahia");
+            listaEstados.Add("Ceará");
+            listaEstados.Add("Distrito Federal");
+            listaEstados.Add("Distrito Federal");
+            listaEstados.Add("Distrito Federal");
+            listaEstados.Add("Espírito Santo");
+            listaEstados.Add("Goiás");
+            listaEstados.Add("Maranhão");
+            listaEstados.Add("Mato Grosso");
+            listaEstados.Add("Mato Grosso do Sul");
+            listaEstados.Add("Minas Gerais");
+            listaEstados.Add("Pará");
+            listaEstados.Add("Paraíba");
+            listaEstados.Add("Paraíba");
+            listaEstados.Add("Paraíba");
+            listaEstados.Add("Paraná");
+            listaEstados.Add("Pernambuco");
+            listaEstados.Add("Piauí");
+            listaEstados.Add("Rio de Janeiro");
+            listaEstados.Add("Rio Grande do Norte");
+            listaEstados.Add("Rio Grande do Sul");
+            listaEstados.Add("Rio Grande do Sul");
+            listaEstados.Add("Rio Grande do Sul");
+            listaEstados.Add("Rondônia");
+            listaEstados.Add("Roraima");
+            listaEstados.Add("Santa Catarina");
+            listaEstados.Add("São Paulo");
+            listaEstados.Add("São Paulo");
+            listaEstados.Add("São Paulo");
+            listaEstados.Add("Sergipe");
+            listaEstados.Add("Tocantins");
+            return listaEstados;
+        }
+    }
+}

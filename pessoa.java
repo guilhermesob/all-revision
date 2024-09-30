583469a171db741e2027f0c5ad275a82ce046fa4
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-09-29 23:01:27 -0300

    Create pessoa.java Aprofundamento de Herança e Polimorfismo em Java

---
+++ pessoa.java
@@ -0,0 +1,27 @@
+//Pacotes
+package com.mycompany.GereEscola;
+
+//Classe
+public class Pessoa {
+	//Atributos
+	protected String nome , nacionalidade , naturalidade;
+	
+	//Métodos
+	public Pessoa ( String nome , String nacionalidade , String naturalidade ) {
+		this.nome = nome;
+		this.nacionalidade = nacionalidade;
+		this.naturalidade = naturalidade;
+	}
+	protected void atualizarNome ( String nome ) {
+		this.nome = nome;
+	}
+	protected String recuperarNome ( ) {
+		return this.nome;
+	}
+	protected String recuperarNacionalidade ( ) {
+		return this.nacionalidade;
+	}
+	protected String recuperarNaturalidade ( ) {
+		return this.naturalidade;
+	}
+}

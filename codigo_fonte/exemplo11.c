dd6c43f7613afe1c61cd99b0158db2550324ce7c
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-10-14 13:21:15 -0300

    Atualizar o exemplo12.c

---
+++ codigo_fonte/exemplo11.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("arquivo virado apenas para fazer o teste da pasta para salvar os arquivos de estrutura de dados!");
+   return 0;
+}
